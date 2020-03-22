bb08c21a9b3bca049220705ca7bbef1b0b35e326
julietaflux <julietaflux@gmail.com>
2020-03-22 20:18:45 -0300

    ÂFix: APIs names

--- api/vets/index.ts
+++ api/vets/index.ts
@@ -3,7 +3,7 @@ import { Methods, Status } from "../../utils/api.utils";
 import { VetsController } from "./_vets.controller";
 import { connectDB } from "../../setup/connect.db";
 
-export default async function HostsApi(req: NowRequest, res: NowResponse) {
+export default async function VetsApi(req: NowRequest, res: NowResponse) {
   await connectDB();
   if (req.method === Methods.Get) {
     if (req.query.id) {
