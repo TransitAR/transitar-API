547af2e81dda32553607797c18130588796cc4c7
GMaiolo <guille.maiolo@gmail.com>
2020-03-31 16:12:34 -0300

    Feat: Refuge by id

--- api/refuges/[id].ts
+++ api/refuges/[id].ts
@@ -3,12 +3,14 @@ import { Methods, Status } from "../../utils/api.utils";
 import { RefugesController } from "./_refuges.controller";
 import { connectDB } from "../../setup/connect.db";
 
-export default async function RefugesApi(req: NowRequest, res: NowResponse) {
+export default async function RefugeApi(req: NowRequest, res: NowResponse) {
   await connectDB();
   if (req.method === Methods.Get) {
-    RefugesController.getRefuges(req, res);
-  } else if (req.method === Methods.Post) {
-    RefugesController.addRefuge(req, res);
+    if (req.query.id) {
+      RefugesController.getRefuge(req, res);
+    } else {
+      res.status(Status.BadRequest).send("Bad request");
+    }
   } else {
     res.status(Status.BadRequest).send("Bad request");
   }
