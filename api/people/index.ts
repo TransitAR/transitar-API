9dc7948fde17ed141cfe62a351001a6bbd8bbd49
julietaflux <julietaflux@gmail.com>
2020-04-02 16:10:16 -0300

    Fix: persons => people

--- api/people/index.ts
+++ api/people/index.ts
@@ -1,6 +1,6 @@
 import { NowRequest, NowResponse } from "@now/node";
 import { Methods, Status } from "../../utils/api.utils";
-import { PeopleController } from "./_persons.controller";
+import { PeopleController } from "./_people.controller";
 import { connectDB } from "../../setup/connect.db";
 
 export default async function PeopleApi(req: NowRequest, res: NowResponse) {
