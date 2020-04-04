654e7d345114e96a4306cd81b5bf6f76601ece7d
GMaiolo <guille.maiolo@gmail.com>
2020-04-04 02:04:37 -0300

    Feat: added patch status and method

--- utils/api.utils.ts
+++ utils/api.utils.ts
@@ -2,11 +2,13 @@ export enum Methods {
   Get = "GET",
   Post = "POST",
   Put = "PUT",
+  Patch = "PATCH",
 }
 
 export enum Status {
   Ok = 200,
   Created = 201,
+  NoContent = 204,
   NotFound = 404,
   BadRequest = 400,
   Error = 500,
